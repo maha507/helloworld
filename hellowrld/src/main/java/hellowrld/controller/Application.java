47c53713553fde357a082176f8a719101735f4b5
maha507 <132257569+maha507@users.noreply.github.com>
2023-11-30 10:32:03 +0530

    changes

--- hellowrld/src/main/java/hellowrld/controller/Application.java
+++ hellowrld/src/main/java/hellowrld/controller/Application.java
@@ -1,11 +1,25 @@
 package hellowrld.controller;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 @SpringBootApplication
 public class Application {
 	 public static void main(String[] args) {
 	        SpringApplication.run(Application.class, args);
+	    }
+
+	    @GetMapping("/hello")
+	    public String hello() {
+	        return "Hello, World!";
+	    }
+
+	    @GetMapping("/display")
+	    public String display() {
+	        return "Welcome !";
 	    }
 
 }
